90f3aca1cc7eb337c14eea271c16d0bf5a638056
Sebastien Lafleur <sebastien.lafleur@gmail.com>
2018-04-25 09:59:05 -0400

    Correction pour parseArgs

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ const {app, BrowserWindow} = require('electron');
 const ArgumentParser = require("argparse").ArgumentParser;
 
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
+	app.quit();
 }
 
 let args;
 let parser = new ArgumentParser({
-    version: require("../package").version,
-    addHelp: true,
-    description: require("../package").description,
+	version: require("../package").version,
+	addHelp: true,
+	description: require("../package").description,
 	prog: require("../package.json").name
 });
 
 parser.addArgument(
-    ['-d', '--debug'],
-    {help: 'Active le mode debug.', defaultValue: false, action: "storeTrue"}
+	['-d', '--debug'],
+	{help: 'Active le mode debug.', defaultValue: false, action: "storeTrue"}
 );
 
 let arg = process.argv.slice(1);
@@ -32,36 +32,36 @@ try {
 let mainWindow;
 
 const createWindow = () => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 1450,
-        height: 900,
-    });
-
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-    if (args.get("debug")) {
-        mainWindow.webContents.openDevTools({mode: "detach"});
-    }
-
-    global.args = args;
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 1450,
+		height: 900,
+	});
+	
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	if (args.debug) {
+		mainWindow.webContents.openDevTools({mode: "detach"});
+	}
+	
+	global.args = args;
+	
+	// Emitted when the window is closed.
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
 };
 
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
